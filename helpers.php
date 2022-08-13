cd7980a82d3c5ab50d3418d0cd97d9f91c5aeb69
SKYFish17 <nwcorp93@yandex.ru>
2022-08-13 12:43:47 +0300

    create db + code refactoring

--- helpers.php
+++ helpers.php
@@ -142,35 +142,3 @@ function include_template($name, array $data = []) {
 
     return $result;
 }
-
-// Считает кол-во задач в проекте
-function count_project_number_of_tasks( $tasks, $project ) {
-    $count_of_tasks = 0;
-
-    foreach( $tasks as $task ) {
-        if ( $task['category'] === $project ) {
-            $count_of_tasks++;
-        }
-    }
-
-    return $count_of_tasks;
-}
-
-// Определяет, срочная ли задача
-function is_urgency_task( $deadline ) {
-    if ( $deadline === 'null' ) {
-        return;
-    }
-
-    $cur_date = date_create( "now" );
-    $deadline_date = date_create( $deadline );
-
-    $diff = date_diff( $cur_date, $deadline_date );
-    $days_count = date_interval_format( $diff, "%r%d" );
-
-    if ( $days_count <= 24 ) {
-        return true;
-    } else {
-        return false;
-    }
-}
