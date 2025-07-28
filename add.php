c749cf77084de6502024cf2011c049f8d974e9a3
d.egorov <d.egorov@quality-lab.ru>
2025-07-28 14:50:14 +0300

    6.8. Получение данных из форм

--- add.php
+++ add.php
@@ -24,43 +24,46 @@ $args = [
 ];
 $form_fields_value = filter_input_array(INPUT_POST, $args);
 
-foreach ($form_fields_value as $key => $field_value) {
-    if ($key === 'name') {
-        $errors[$key] = validateEmpty($field_value);
-    } elseif ($key === 'project') {
-        $errors[$key] = validateProject($link, $user_id, $field_value);
-    } elseif ($key === 'date') {
-        $errors[$key] = validateDate($field_value);
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    foreach ($form_fields_value as $key => $field_value) {
+        if ($key === 'name') {
+            $errors[$key] = validateEmpty($field_value);
+        } elseif ($key === 'project') {
+            $errors[$key] = validateProject($link, $user_id, $field_value);
+        } elseif ($key === 'date') {
+            $errors[$key] = validateDate($field_value);
+        }
     }
-}
 
-// Сохранение файла
-$file = $_FILES['file'] ?? '';
-$saved_file_path = null;
+    // Сохранение файла
+    $file = $_FILES['file'] ?? '';
+    $saved_file_path = null;
 
-if (!empty($file['name'])) {
-    $saved_file_path = saveFile($file);
-//    // Доп. функционал с валидацией не по ТЗ
-//    $errors['file'] = validateFile($file);
-//
-//    if (empty($errors['file'])) {
-//        saveFile($file);
-//    }
-}
+    if (!empty($file['name'])) {
+        $saved_file_path = saveFile($file);
 
-$errors = array_filter($errors, function ($elem) { return !empty($elem); });
+        // Доп. функционал с валидацией не по ТЗ
+        $errors['file'] = validateFile($file);
 
-if (empty($errors) && !empty($form_fields_value)) {
-    if (createTask(
-        $link,
-        $form_fields_value['name'],
-        $form_fields_value['project'],
-        !empty($form_fields_value['date']) ? $form_fields_value['date'] : null,
-        $saved_file_path ?? null,
-        $user_id
-    )) {
-        header("Location: /", true, 301);
-        exit();
+        if (empty($errors['file'])) {
+            saveFile($file);
+        }
+    }
+
+    $errors = array_filter($errors, function ($elem) { return !empty($elem); });
+
+    if (empty($errors) && !empty($form_fields_value)) {
+        if (createTask(
+            $link,
+            $form_fields_value['name'],
+            $form_fields_value['project'],
+            !empty($form_fields_value['date']) ? $form_fields_value['date'] : null,
+            $saved_file_path ?? null,
+            $user_id
+        )) {
+            header("Location: /", true, 301);
+            exit();
+        }
     }
 }
 
